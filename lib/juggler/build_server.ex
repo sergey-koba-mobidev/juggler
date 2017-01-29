e2395b09b3d4676c5946cffdcd3430abe0d13362
Sergey Koba <s.koba@mobidev.biz>
2017-01-29 23:03:46 +0200

    build_id -> build.id

--- lib/juggler/build_server.ex
+++ lib/juggler/build_server.ex
@@ -19,46 +19,46 @@ defmodule Juggler.BuildServer do
     build = Build |> Repo.get!(build_id) |> Repo.preload([:project])
     commands_arr = String.split(build.project.build_commands, "\n")
     Logger.info " ---> Started build " <> Integer.to_string(build_id) <> " commands: " <> inspect(commands_arr)
-    spawn fn -> exec_commands(build.id, commands_arr) end
+    spawn fn -> exec_commands(build, commands_arr) end
     {:noreply, [build_id | state]}
   end
 
-  def exec_commands(build_id, commands) do
+  def exec_commands(build, commands) do
     case List.pop_at(commands, 0) do
       {nil, []} ->
-        process_build_output(build_id, "cmd_finished", %{})
+        process_build_output(build, "cmd_finished", %{})
       {cmd, cmds} ->
-        {:ok, status} = exec_command(build_id, String.trim(cmd, "\r"))
+        {:ok, status} = exec_command(build, String.trim(cmd, "\r"))
         case status == 0 do
-          true  -> exec_commands(build_id, cmds)
-          false -> process_build_output(build_id, "cmd_finished_error", %{})
+          true  -> exec_commands(build, cmds)
+          false -> process_build_output(build, "cmd_finished_error", %{})
         end
     end
   end
 
-  def exec_command(build_id, command) do
-    Logger.info " ---> Executing build " <> Integer.to_string(build_id) <> " cmd: " <> inspect(command)
-    process_build_output(build_id, "cmd_start", %{cmd: command})
+  def exec_command(build, command) do
+    Logger.info " ---> Executing build " <> Integer.to_string(build.id) <> " cmd: " <> inspect(command)
+    process_build_output(build, "cmd_start", %{cmd: command})
     # TODO: refactor to streams
     %Result{out: output, status: status} = Porcelain.shell(command, err: :out)
-    process_build_output(build_id, "cmd_data", %{output: output, cmd: command})
-    process_build_output(build_id, "cmd_result", %{status: status, cmd: command})
-    Logger.info " ---> Finished build " <> Integer.to_string(build_id) <> " cmd: " <> inspect(command) <> " result: " <> Integer.to_string(status)
+    process_build_output(build, "cmd_data", %{output: output, cmd: command})
+    process_build_output(build, "cmd_result", %{status: status, cmd: command})
+    Logger.info " ---> Finished build " <> Integer.to_string(build.id) <> " cmd: " <> inspect(command) <> " result: " <> Integer.to_string(status)
     {:ok, status}
   end
 
-  def process_build_output(build_id, event, payload) do
+  def process_build_output(build, event, payload) do
     changeset = BuildOutput.changeset(%BuildOutput{}, %{
-      :build_id => build_id,
+      :build_id => build.id,
       :event => event,
       :payload => payload
     })
 
     case Repo.insert(changeset) do
       {:ok, build} ->
-        Juggler.Endpoint.broadcast("build:" <> Integer.to_string(build_id), event, payload)
+        Juggler.Endpoint.broadcast("build:" <> Integer.to_string(build.id), event, payload)
       {:error, _changeset} ->
-        Logger.error "Error inserting BuildOutput for build " <> Integer.to_string(build_id)
+        Logger.error "Error inserting BuildOutput for build " <> Integer.to_string(build.id)
     end
   end
 end
